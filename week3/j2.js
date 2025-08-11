6ac86a89b31d79cf3fcb1d2e4cf88928b61a8c03
APPALA MEENAKSHI SINDHUJA <146017219+appalameenakshisindhuja@users.noreply.github.com>
2025-08-11 10:44:59 +0530

    Create j2.js

---
+++ week3/j2.js
@@ -0,0 +1,11 @@
+const readline = require("readline");
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+rl.question("Enter a number: ", (num) => {
+    num = parseInt(num);
+    console.log(`Multiplication Table of ${num}`);
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${num} x ${i} = ${num * i}`);
+    }
+    rl.close();
+});
